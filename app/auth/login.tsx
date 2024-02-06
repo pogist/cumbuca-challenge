d48898786aa01cd0fe9add7654868a728c8fdb9c
pogist <murilo.paixao.2@gmail.com>
2024-02-06 19:43:19 -0300

    feat(auth): bootstrap login screen UI

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,9 +1,28 @@
-import { View, Text, StyleSheet } from 'react-native';
+import Button from '@components/Button';
+import TextField from '@components/TextField';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Login Page</Text>
+      <Text style={styles.header}>Login</Text>
+      <View style={styles.form}>
+        <TextField label="CPF" placeholder="Ex.: 000.000.000-00" error={null} />
+        <TextField
+          label="Senha"
+          placeholder="Digite sua senha..."
+          error={null}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Button
+          label="LOGIN"
+          labelStyle={styles.loginLabel}
+          containerStyle={styles.loginContainer}
+        />
+        <Button label="Criar conta" />
+      </View>
     </View>
   );
 }
@@ -12,6 +31,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 20,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: '600',
+  },
+  form: {
+    gap: 16,
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+  },
+  footer: {
+    gap: 16,
+    alignSelf: 'stretch',
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  loginLabel: {
+    color: 'white',
+    fontSize: 16,
+  },
+  loginContainer: {
+    height: 42,
+    borderRadius: 10,
+    alignSelf: 'stretch',
+    backgroundColor: 'black',
   },
 });
