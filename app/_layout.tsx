b1a0ff261c3661e690aa18e82674c8ef2a12da5a
pogist <murilo.paixao.2@gmail.com>
2024-02-08 15:26:31 -0300

    feat(icons): preload icons as soon as possible

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,19 @@
+import { Octicons } from '@expo/vector-icons';
 import { ThemeProvider, darkTheme, lightTheme } from '@theme';
+import * as Font from 'expo-font';
 import { Slot } from 'expo-router';
+import React from 'react';
 import { useColorScheme } from 'react-native';
+
+async function preloadIcons() {
+  await Font.loadAsync({ ...Octicons.font });
+}
 
 export default function RootLayout() {
   const scheme = useColorScheme();
+  React.useEffect(() => {
+    preloadIcons();
+  }, []);
   return (
     <ThemeProvider theme={scheme === 'dark' ? darkTheme : lightTheme}>
       <Slot />
