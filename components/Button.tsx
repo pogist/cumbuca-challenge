64b4448728137db34f0b56221dfc7d2b3385e233
pogist <murilo.paixao.2@gmail.com>
2024-02-11 16:14:12 -0300

    refactor(components): avoid Button re-render by parent and onPress

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,35 +15,49 @@ export interface ButtonProps {
   containerStyle?: ViewStyle;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  label,
-  onPress,
-  disabled,
-  labelStyle: overrideLabelStyle,
-  containerStyle: overrideContainerStyle,
-}) => {
-  const labelStyle = !overrideLabelStyle
-    ? styles.label
-    : [styles.label, overrideLabelStyle];
-
-  const containerStyle = ({ pressed }: { pressed: boolean }) => {
-    const style: ViewStyle[] = !overrideContainerStyle
-      ? [styles.container]
-      : [styles.container, overrideContainerStyle];
-    if (disabled) {
-      style.push(styles.disabled);
-    } else if (pressed) {
-      style.push(styles.pressed);
-    }
-    return style;
-  };
-
+function arePropsEqual(prev: ButtonProps, next: ButtonProps) {
   return (
-    <Pressable style={containerStyle} onPress={onPress} disabled={disabled}>
-      <Text style={labelStyle}>{label}</Text>
-    </Pressable>
+    prev.label === next.label &&
+    prev.disabled === next.disabled &&
+    Object.is(prev.labelStyle, next.labelStyle) &&
+    Object.is(prev.containerStyle, next.containerStyle)
   );
-};
+}
+
+const Button: React.FC<ButtonProps> = React.memo(
+  ({
+    label,
+    onPress,
+    disabled,
+    labelStyle: overrideLabelStyle,
+    containerStyle: overrideContainerStyle,
+  }) => {
+    const labelStyle = !overrideLabelStyle
+      ? styles.label
+      : [styles.label, overrideLabelStyle];
+
+    const containerStyle = ({ pressed }: { pressed: boolean }) => {
+      const style: ViewStyle[] = !overrideContainerStyle
+        ? [styles.container]
+        : [styles.container, overrideContainerStyle];
+      if (disabled) {
+        style.push(styles.disabled);
+      } else if (pressed) {
+        style.push(styles.pressed);
+      }
+      return style;
+    };
+
+    return (
+      <Pressable style={containerStyle} onPress={onPress} disabled={disabled}>
+        <Text style={labelStyle}>{label}</Text>
+      </Pressable>
+    );
+  },
+  arePropsEqual,
+);
+
+Button.displayName = 'Button';
 
 const styles = StyleSheet.create({
   container: {
